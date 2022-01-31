7e4893ed86378a9196add17e40b8783c356ab3ec
Samrath Singh Kalra <samrathsinghrohit@gmail.com>
2022-01-31 07:46:41 +0530

    Minor changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import profile from './Assets/samrath cropped.jpg'
+import cv from './Assets/Frontend-Developer CV.pdf'
 
 const About = () => {
     return (
@@ -10,7 +11,7 @@ const About = () => {
             <div className="aboutdiv2">
                 <div className="text6">I'm Samrath and I am a Learner</div>
                 <p className="text7">I am a Microsoft Learn Student Ambassador (Alpha) | Full Stack Web Development Intern @TwoWaits | DSA Scholar Intern @TwoWaits | Public Speaker and Content Writer @Ojaswi-LPU | B.Tech CSE 24' @Lovely Professional University | Coder | Learner | Achiever<br/><br/><span className="visit">Know more from my CV below.</span></p>
-                <a href="https://www.goodcv.com/cv/d985d5475e6db0fbbd8e8520ee936711" target="_blank"><button className="button1">Open CV</button></a>
+                <a href={cv} target="_blank"><button className="button1">Open CV</button></a>
             </div>
         </div>
     </section>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,21 +12,21 @@ const Projects = () => {
                 <div className="block1">
                     <img className="proj1" src={breakingbad}/>
                     <div className="text9">Breaking Bad Wiki</div>
-                    <p className="text10">This project is a Wikipedia of Breaking Bad, one of the most popular TV Series. It has been prepared using HTML, CSS, JavaScript and React JS under Corporate Experience Program of TwoWaits, supervised by Mr. Sanidhya Panwar.<br/><br/><span className="visit">Click To Learn More</span></p>
+                    <p className="text10">A fully responsive, frontend-based web application of the popular American TV series Breaking Bad. Implements React APIs to fetch pictorial and descriptive data of all the characters and provides detailed insights to the user<br/><br/><span className="visit">Click To Learn More</span></p>
                 </div>
             </a>
-            <a href="https://hitman-samrath.github.io/Portfolio-Website1/new.html" target="_blank">
+            <a href="https://hitman-samrath.github.io/NotesApp" target="_blank">
                 <div className="block2">
-                    <img className= "proj2" src={instaclone}/>
-                    <div className="text9">Instagram Clone</div>
-                    <p className="text10">This project is a clone web application of social media application Instagram. It has been prepared using HTML, CSS, JavaScript, React JS, Mongo DB and APIs under Corporate Experience Program of TwoWaits, supervised by Mr. Sanidhya Panwar.<br/><br/><span className="visit">Click To Learn More</span></p>
+                    <img className= "proj2" src="https://media.istockphoto.com/photos/to-do-list-text-on-notepad-picture-id1285308242?b=1&k=20&m=1285308242&s=170667a&w=0&h=K-UO77yYEQAKjRNLhgXADlyRIqrPkB8sC-4mKcyMgC4="/>
+                    <div className="text9">Samrath Notes App</div>
+                    <p className="text10">A fully responsive, frontend-based notes making application, with UI similar to Google Keep. Create notes with the title and body and dispense them in real time using components of ReactJs. Alerts are generated upon adding empty notes.<br/><br/><span className="visit">Click To Learn More</span></p>
                 </div>
             </a>
             <a href="https://hitman-samrath.github.io/Portfolio-Website1/new.html" target="_blank">
                 <div className="block3">
                     <img className="proj3" src={internshipnow}/>
                     <div className="text9">Internship Now</div>
-                    <p className="text10">This project is a professional platform for connecting recruiters with potential candidates. It has been prepared using HTML, CSS, MERN Stack and APIs under Corporate Experience Program of TwoWaits, supervised by Mr. Sanidhya Panwar.<br/><br/><span className="visit">Click To Learn More</span></p>
+                    <p className="text10">This project is a professional platform for connecting recruiters with potential candidates, and helping them through the process. It has been prepared using MERN Stack and APIs under the management of Mr. Sanidhya Panwar.<br/><br/><span className="visit">Click To Learn More</span></p>
                 </div>
             </a>
         </div>
